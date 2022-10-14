bcde728262781923e8a1e1068fba52878c439ba5
Fetaksa <rustykami@yandex.ru>
2022-10-14 16:05:34 +0300

    Task03 completed

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа
+// в промежутке от -N до N.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int count = -number;
+    while (count <= number)
+    {
+        Console.Write($"{count} ");
+        count++;
+        // count = count + 1;
+    }
+}
+
+else Console.WriteLine("Введено некорректное число. Введите целое положительное число.");
